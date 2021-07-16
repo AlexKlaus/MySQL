69ff30555beee96943df333be3bf0c739d9c3379
Alex <krounix@gmail.com>
2021-07-16 08:26:59 +0300

    выполнил практические задания

---
+++ task-3/media_types.sql
@@ -0,0 +1,3 @@
+insert into media_types (name, created_at, updated_at) values ('photo', '2021-01-04 21:14:42', '2021-04-01 04:11:07');
+insert into media_types (name, created_at, updated_at) values ('audio', '2021-01-19 15:29:19', '2021-01-08 15:14:51');
+insert into media_types (name, created_at, updated_at) values ('video', '2021-06-13 21:38:07', '2021-01-17 01:44:35');
