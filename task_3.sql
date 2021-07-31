58489730f2aa37009a3fb0de0d45333fe04085db
Alex <krounix@gmail.com>
2021-07-31 17:42:37 +0300

    добавил решение в третью задачу

--- task_3.sql
+++ task_3.sql
@@ -1,3 +1,12 @@
+
+-- без join
 SELECT (SELECT name FROM cities where `from`=label) AS `from`, 
 	(SELECT name FROM cities WHERE `to`=label) AS `to` 
 	FROM flights;
+
+
+-- join
+SELECT c.name `from`, c2.name `to` FROM flights f
+	JOIN cities c ON f.`from` = c.label
+	JOIN cities c2 ON f.`to` = c2.label
+	ORDER BY f.id;
